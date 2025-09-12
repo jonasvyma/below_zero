5ea68c3f7c3821b193a8b3686cad398f01a625a6
Jonas Vyshniauskas <jonasvyma@gmail.com>
2025-09-12 15:34:11 +0200

    staging and prep models 2

--- dbt/models/staging/staging_flights.sql
+++ dbt/models/staging/staging_flights.sql
@@ -1,7 +1,7 @@
 
 WITH flights AS (
     SELECT * 
-    FROM {{source('flights_data', 'flights')}}
+    FROM {{source('flights_data', 'flights_raw')}}
 )
 SELECT * FROM flights
 WHERE origin in ('MSY', 'MOB', 'MIA') OR dest in ('MSY', 'MOB', 'MIA')
